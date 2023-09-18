304a62b4a82a1f1273ec6cd6c6c2b927df18f586
thekaiofc <kaio.hen13@gmail.com>
2023-09-18 20:28:07 -0300

    teste

--- app-exercicio-1/App.js
+++ app-exercicio-1/App.js
@@ -1,10 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Contador from "./components/Contador";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Contador />
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
